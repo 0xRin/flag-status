6022bc2f9b6d4519b0e82dd821bad4c8765c876e
0xRin <73491185+0xRin@users.noreply.github.com>
2023-10-23 21:50:07 -0400

    feat(core): parse flag status text from website

--- index.ts
+++ index.ts
@@ -1,18 +1,39 @@
 // library imports
-import express, {Express, Request, Response} from 'express';
-import dotenv from 'dotenv';
+import express, { Express, Request, Response } from "express";
+import dotenv from "dotenv";
+import puppeteer from "puppeteer";
 
 // initialize express app
-const app : Express = express();
+const app: Express = express();
 dotenv.config();
 
 // define port
 const PORT = process.env.PORT || 3000;
 
-app.get('/', (req: Request, res: Response) => {
-    res.status(200).send('🔥 It\'s working');
-});
+// get flag status text
+const getFlagStatus = async () => {
+  // Launch the browser and open a new blank page
+  const browser = await puppeteer.launch({ headless: "new" });
+  const page = await browser.newPage();
+
+  // Navigate the page to a URL
+  await page.goto(process.env.TARGET_SITE as string);
+  // grab the element w/ given html selectors
+  const textSelector = await page.waitForSelector(".disclose h2");
+  const flagStatusTag = await textSelector?.evaluate((el) => el.textContent);
+
+  // format the text
+  const flagStatusText = flagStatusTag?.replace(/\s+/g, "").toUpperCase();
+
+  console.log(flagStatusText);
+};
+
+try {
+  getFlagStatus();
+} catch (e: Error | unknown) {
+  if (e instanceof Error) console.log(e.message);
+}
 
 app.listen(PORT, () => {
-    console.log(`😎[server]: Server is running on port ${PORT}`)
+  console.log(`😎[server]: Server is running on port ${PORT}`);
 });
